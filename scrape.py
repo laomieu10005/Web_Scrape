c6e84684c65d4e2d8a9c2f35a884d5c36c972554
LaoMi3uu <thanhchu.ict@gmail.com>
2023-02-19 10:19:02 +0700

    update data

--- scrape.py
+++ scrape.py
@@ -1,40 +1,33 @@
-import os,sys
+import os,sys,time
+from datetime import datetime
 try:
     from selenium import webdriver
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.common.by import By
 except:    
     os.system("pip install selenium")
-    from selenium import webdriver
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.common.by import By
-try:
-    from chromedriver_py import binary_path # this will get you the path variable
-except:    
-    os.system("pip install chromedriver_py")
-    from chromedriver_py import binary_path # this will get you the path variable
 
 
-service_object = Service(binary_path)
-driver = webdriver.Chrome(service=service_object)
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-username = "user"
-password = "password"
-url = ""
-data_folder = './Data'
+driver = webdriver.ChromiumEdge()
+username = "automated"
+password = "automatedautomated"
+url = "http://automated.pythonanywhere.com/login/"
 
 driver.get(url)
-driver.find_element("id", "login_field").send_keys("user")
-driver.find_element("id", "password_field").send_keys("password")
-driver.find_element("name", "commit").click()
-WebDriverWait(driver=driver, timeout=20).until(
-    lambda x: x.execute_script("return document.readyState === 'complete'")
-)
-error_message = "Incorrect username or password."
-errors = driver.find_elements(By.CLASS_NAME, "flash-error")
+time.sleep(2) # stay on the page within 2 sec
+driver.find_element("id", "id_username").send_keys(username)
+driver.find_element("id", "id_password").send_keys(password+Keys.RETURN)
 
-if any(error_message in e.text for e in errors): 
-    print("[!] Login failed")
-else:
-    print("[+] Login successful")
+time.sleep(2) # stay on the page within 2 sec
+element = driver.find_element(by="xpath", value="/html/body/div[1]/h1[2]")
+print(element.text)
+now=datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+data_file = open(f"Data\data_file_{now}.txt",'w')
+data_file.write(element.text)
+data_file.close()
+
 driver.close()    
